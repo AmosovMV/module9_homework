a50b90b70de7ccb4d0638543c63819761ee6b027
Maksim <82663071+AmosovMV@users.noreply.github.com>
2021-09-03 16:01:20 +0300

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -1,15 +1,12 @@
-const btn = document.getElementById("btn");
-const inWidth = document.getElementById("width").value;
-const inHeight = document.getElementById("heigth").value;
-
-//btn.onclick = function() {
- function res(){ 
-  if (inWidth<100 || inWidth>300) {
+btn.onclick = function() {
+  document.querySelector(".output").innerHTML='<div></div>'
+  const btn = document.getElementById("btn");
+  const inWidth = document.getElementById("width").value;
+  const inHeight = document.getElementById("heigth").value; 
+ if (inWidth<100 || inWidth>300 || inHeight<100 || inHeight>300) {
     document.querySelector(".output").innerHTML='<div>Неверный диапазон</div>'
   }  else {
     console.log('Succses' + " " + inWidth +  " " + inHeight);
   }
 
 }
-
-btn.addEventListener('click', res);
